3f91c1739a837338dc1d272446cde939c7c4e172
Sean Shanley <seanshanley@umail.ucsb.edu>
2021-10-08 09:19:58 -0700

    another thing that will crash

--- tutorial.js
+++ tutorial.js
@@ -1,8 +1,8 @@
 var http = require('http');
-
+const PORT = process.env.PORT || 4000;
 http.createServer(function (req,res){
 	res.write('Hello World!');
 	res.end();
-}).listen(7070);
+}).listen(PORT, () => console.log('Listening on ${ PORT }'));
 //let z = 19*97;
 //<button onclick=alert(z)>Free number right here</button>
