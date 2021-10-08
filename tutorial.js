11f4ab9bd7fc40f6685d301fef33e20a796bb0cb
Sean Shanley <seanshanley@umail.ucsb.edu>
2021-10-08 09:09:04 -0700

    apparently this will work

--- tutorial.js
+++ tutorial.js
@@ -1,2 +1,8 @@
-let z = 19*97;
-<button onclick=alert(z)>Free number right here</button>
+var http = require('http');
+
+http.createServer(function (req,res){
+	res.write('Hello World!');
+	res.end();
+}).listen(7070);
+//let z = 19*97;
+//<button onclick=alert(z)>Free number right here</button>
